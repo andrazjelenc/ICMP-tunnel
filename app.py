7645371ecff4d64a51ca88c50da88c7398db96a8
Andraž Jelenc <andraz.jelenc@siol.net>
2017-05-06 10:33:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import dpkt
 import os
 import random
 import select
@@ -7,28 +6,52 @@ import struct
 import sys
 import threading
 
+def checksum(source_string):
+    sum = 0
+    countTo = (len(source_string)/2)*2
+    count = 0
+    while count<countTo:
+        thisVal = ord(source_string[count + 1])*256 + ord(source_string[count])
+        sum = sum + thisVal
+        sum = sum & 0xffffffff
+        count = count + 2
+
+    if countTo<len(source_string):
+        sum = sum + ord(source_string[len(source_string) - 1])
+        sum = sum & 0xffffffff
+
+    sum = (sum >> 16)  +  (sum & 0xffff)
+    sum = sum + (sum >> 16)
+    answer = ~sum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+
+    return answer
+
 def send_message(inputData):
 	splitted = inputData.split(' ', 1)
 	
 	dest = socket.gethostbyname(splitted[0]) #destination IP addr
-	msg = splitted[1].encode("utf-8") #message
+	message = splitted[1].encode("utf-8") #encoded message
 	
-	#dpkt object
-	echo = dpkt.icmp.ICMP.Echo()
-	echo.id = random.randint(0, 0xffff)
-	echo.seq = random.randint(0, 0xffff)
-	echo.data = msg
-
-	icmp = dpkt.icmp.ICMP()
-	icmp.type = dpkt.icmp.ICMP_ECHO 
-	icmp.data = echo
+	my_checksum = 0
+	
+	id = random.randint(1, 0xffff)
+	
+	# Header is type (8), code (8), checksum (16), id (16), sequence (16)
+	header = struct.pack("bbHHh", 8, 0, my_checksum, id, 1)
+	
+	my_checksum = checksum(header + message)
+	
+	header = struct.pack("bbHHh", 8, 0, socket.htons(my_checksum), id, 1)
+	packet = header + message
 
 	#open socket
-	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, dpkt.ip.IP_PROTO_ICMP)
+	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
 	
 	#bind to public interface (ip, port) and send packet
 	s.bind((INTERFACE, 0)) 
-	s.sendto(str(icmp), (dest, 1))
+	s.sendto(str(packet), (dest, 1))
 	
 
 def receive_message(sniffer):
@@ -67,7 +90,7 @@ def message_receiver():
 
 if __name__ == "__main__":
 	global INTERFACE
-	INTERFACE = "192.168.1.10" #default interface, used if input empty
+	INTERFACE = "192.168.1.12" #default interface, used if input empty
 
 	newIface = raw_input("Enter interface ip: ")
 	if(len(newIface) > 0):
@@ -78,7 +101,7 @@ if __name__ == "__main__":
 	thread.daemon = True
 	thread.start()
 
-	#send message (format: "destination message")
+	#send message (format: "<destination> <message>")
 	while True:
 		try:
 			msg_send = raw_input()
