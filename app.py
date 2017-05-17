604ce4148acef8f68b2c7f5cbe3668a04c814c6a
Andraž Jelenc <andraz.jelenc@siol.net>
2017-05-17 11:13:06 +0200

    Update app.py
    Added some try catchs

--- app.py
+++ app.py
@@ -97,8 +97,8 @@ def receive_message(sniffer):
 	type, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
 	
 	#filter out our own packets
-	if(addr[0] == INTERFACE): 
-		return ""
+	#if(addr[0] == INTERFACE): 
+	#	return ""
 	
 	if(type == 0):
 		type = "Reply"
@@ -109,7 +109,7 @@ def receive_message(sniffer):
 	
 	msgData = icmpData#.decode("utf-8")
 	
-	if execMode == True and len(msgData) > 5 and msgData[:5] == "#run#":#and type == "Request" #be carefull without that one
+	if EXECMODE == True and len(msgData) > 5 and msgData[:5] == "#run#":#and type == "Request" #be carefull without that one
 		execution(addr[0], msgData[5:])
 	
 	message = "[" + addr[0] + "] [" + type + "]: " + msgData
@@ -118,43 +118,66 @@ def receive_message(sniffer):
 
 def message_receiver():
 	#open socket and bind it to our interface
-	sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
-	sniffer.bind((INTERFACE, 0))
 	
-	#capture headers too
-	sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+	#todo: tukaj se se nekaj sesuva, lahko tudi v funkciji reveive_message
+	#try:
+		sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+		sniffer.bind((INTERFACE, 0))
+	
+		#capture headers too
+		sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-	#on WinOS we need to enable promisc mode
-	if os.name == "nt":
-		sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-	while True:
-		msg_received = receive_message(sniffer)
-		if len(msg_received) > 0: # ce obstaja sporocilo
-			print("[Received] " + msg_received) # izpisi
-
+		#on WinOS we need to enable promisc mode
+		if os.name == "nt":
+			sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+		
+		while True:
+			msg_received = receive_message(sniffer)
+			if len(msg_received) > 0: # ce obstaja sporocilo
+				print("[Received] " + msg_received) # izpisi
+				
+	#except:
+	#	print "[!] FATAL ERROR"
+	#	print "Try running script as administrator and test correctness of your interface IP"
+	#	print "Dying now..."
+	#	os._exit(1)
 
 if __name__ == "__main__":
 	global INTERFACE
-	global execMode
+	global EXECMODE
 	
-	INTERFACE = "192.168.1.12" #default interface, used if input empty
-	execMode = True		#run commands in cmd (messages: #run#command)		
+	#INTERFACE = "192.168.1.12" #default interface, used if input empty
+	INTERFACE = "193.2.179.239"
+	EXECMODE = False		#run commands in cmd (messages: #run#command)		
 	
 	newIface = raw_input("Enter interface ip: ")
 	if(len(newIface) > 0):
 		INTERFACE = newIface
 	
+	newExecMode = raw_input("Enable command execution (ExecMode)? [YES/any]")
+	if(len(newExecMode) > 0 and newExecMode.strip() == "YES"):
+		EXECMODE = True
+	
+	if(EXECMODE == True):
+		print "Using interface IP: " + INTERFACE + " with ExecMode ENABLED!"
+	else:
+		print "Using interface IP: " + INTERFACE + " with ExecMode disabled!"
+		
+	
 	#start listening on interface
 	thread = threading.Thread(target=message_receiver)
 	thread.daemon = True
 	thread.start()
-
+	
+	print "Sniffer started! Waiting..."
+	
 	#send message (format: "<destination> <message>")
 	while True:
 		try:
-			msg_send = raw_input()
+			msg_send = raw_input(">")
 			parts = msg_send.split(' ', 1)
 			send_message(parts[0], parts[1])
+		except IndexError:
+			print "[!]Input format is: <desination URL/IP> <message>"
 		except KeyboardInterrupt:
 			sys.exit()
