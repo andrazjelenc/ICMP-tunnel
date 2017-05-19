032c9d05ee034ec22a8fc8c52e683d6f81797e51
Andraž Jelenc <andraz.jelenc@siol.net>
2017-05-19 09:51:37 +0200

    Update app.py
    Fixed packet lenght

--- app.py
+++ app.py
@@ -96,15 +96,12 @@ def send_message(dest, message):
 
 def receive_message(sniffer):
 	whatReady = select.select([sniffer], [], [], 2)
-	recPacket, addr = sniffer.recvfrom(1024)
+	recPacket, addr = sniffer.recvfrom(65565)
 	icmpHeader = recPacket[20:28]
 	icmpData = recPacket[28:]
 	type, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
 	
 	source = addr[0]
-	#filter out our own packets
-	if(source == INTERFACE): 
-		source =  str(source) + " (me)"	#in this case disable execMode
 	
 	if(type == 0):
 		type = "Reply"
@@ -112,21 +109,25 @@ def receive_message(sniffer):
 		type = "Request"
 	else:
 		type = "Unknown("+type+")"
-	
+		
 	msgData = icmpData#.decode("utf-8")
 	
-	if EXECMODE == True and len(msgData) > 5 and msgData[:5] == "#run#":#and type == "Request" #be carefull without that one, what about source=="me"
+	#filter out our own packets
+	if(source == INTERFACE): 
+		return ""
+	
+	if EXECMODE == True and len(msgData) > 5 and msgData[:5] == "#run#" and type == "Request": #be carefull without that one
 		execution(source, msgData[5:])
 	
-	message = "[" + source + "] [" + type + "]: " + msgData
-	return message
-
+	return "[Received] [" + str(source) + "] [" + str(type) + "]: " + str(msgData)
 
 def message_receiver():
 	#open socket and bind it to our interface
 	
 	#todo: tukaj se se nekaj sesuva, lahko tudi v funkciji reveive_message
 	try:
+		print "Starting sniffer..."
+		
 		sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
 		sniffer.bind((INTERFACE, 0))
 	
@@ -137,19 +138,24 @@ def message_receiver():
 		if os.name == "nt":
 			sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 		
+		print "Sniffer started! Waiting..."
+		
 		while True:
 			msg_received = receive_message(sniffer)
 			if len(msg_received) > 0: # ce obstaja sporocilo
 				print(msg_received) # izpisi
 				
 	except Exception as inst:
-		if(inst.args[0] == 10049 or inst.args[0] == 11004):
+		if(inst.args[0] == 10049 or inst.args[0] == 11004 or inst.args[0] == 99):
 			print "Unable to access local interface - bad IP"
 		elif(inst.args[0] == 10013):
 			print "Unable to access local interface - run as root"
 		else:
 			print inst.args
 		
+		if os.name == "nt":
+			sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+			
 		os._exit(1)
 
 if __name__ == "__main__":
@@ -178,13 +184,11 @@ if __name__ == "__main__":
 	thread = threading.Thread(target=message_receiver)
 	thread.daemon = True
 	thread.start()
-	
-	print "Sniffer started! Waiting..."
-	
+		
 	#send message (format: "<destination> <message>")
 	while True:
 		try:
-			msg_send = raw_input(">")
+			msg_send = raw_input()
 			parts = msg_send.split(' ', 1)
 			send_message(parts[0], parts[1])
 		except IndexError:
